1bbb57932a4d1c9670801f91fb948452f9616b05
Peter <72401515+Porkchop414@users.noreply.github.com>
2021-12-05 15:04:47 -0500

    Distance Data
    Updated Distance Data file, load distance data function now correctly loads data from csv to a matrix.

--- .idea/Main/Distance_Data.py
+++ .idea/Main/Distance_Data.py
@@ -1,19 +1,18 @@
 import csv
+
 matrix = []
-for i in range(27):
-    matrix.append([0] * 27)
 
 def loadDistanceData(file_name):
     with open(file_name) as ourDistances:
         distance_data = csv.reader(ourDistances, delimiter=',')
-        next(distance_data) # skip header
-        for hub in distance_data:
-            for row in range(27):
-                for column in range(27):
-                    matrix[row][column] = hub[column]
+        next(distance_data)
+        for item in distance_data:
+            matrix.append(item)
+
     return matrix
 
 def printDistanceMatrix(matrix):
-    for row, col in range(len(matrix)):
-        print(matrix[row][col])
+    for item in matrix:
+        print(item)
+
 
--- .idea/Main/Main.py
+++ .idea/Main/Main.py
@@ -5,11 +5,15 @@ from Distance_Data import loadDistanceData, printDistanceMatrix
 # Hash table instance
 myHash = ChainingHashTable()
 
+# Matrix holding all the distance data
+myMatrix = loadDistanceData('C:/Users/Peter/IdeaProjects/C950_Package_Delivery/.idea/CSV_Files/WGUPS Distance Table.csv')
+
 # Load packages to Hash Table
-loadPackageData('CSV_Files/WGUPS Package File.csv', myHash)
-printDistanceMatrix(loadDistanceData('CSV_Files/WGUPS Distance Table.csv'))
+loadPackageData('C:/Users/Peter/IdeaProjects/C950_Package_Delivery/.idea/CSV_Files/WGUPS Package File.csv', myHash)
+printDistanceMatrix(myMatrix)
+
 
 print("Packages from Hashtable:")
 # Fetch data from Hash Table
-#for i in range (int((getNumberOfItemsFromMyHash(myHash.table))/2)):
- #   print("Package: {}".format(myHash.search(i+1))) # 1 to 11 is sent to myHash.search()
+for i in range (int((getNumberOfItemsFromMyHash(myHash.table))/2)):
+    print("Package: {}".format(myHash.search(i+1))) # 1 to 11 is sent to myHash.search()
