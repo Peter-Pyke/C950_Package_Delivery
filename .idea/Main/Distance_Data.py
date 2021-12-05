7670215fcc963ab969d19dbab09826d7b1f7ce0d
Peter <72401515+Porkchop414@users.noreply.github.com>
2021-12-05 15:59:06 -0500

    updated Package class
    added a getaddress function and also started a address_look_up function in distance_data file

--- .idea/Main/Distance_Data.py
+++ .idea/Main/Distance_Data.py
@@ -15,4 +15,12 @@ def printDistanceMatrix(matrix):
     for item in matrix:
         print(item)
 
+def address_look_up(myHash):
+    address_key_value = {}
+    for i in range(0, 27):
+        first_package_data = myHash.search(i)
+        address_key_value[first_package_data.get_address()] = i
+    return address_key_value
+
+
 
--- .idea/Main/Main.py
+++ .idea/Main/Main.py
@@ -1,4 +1,4 @@
-from Package_Data import loadPackageData
+from Package_Data import loadPackageData, Package
 from My_Hash_Table import getNumberOfItemsFromMyHash, ChainingHashTable
 from Distance_Data import loadDistanceData, printDistanceMatrix
 
@@ -6,14 +6,20 @@ from Distance_Data import loadDistanceData, printDistanceMatrix
 myHash = ChainingHashTable()
 
 # Matrix holding all the distance data
-myMatrix = loadDistanceData('C:/Users/Peter/IdeaProjects/C950_Package_Delivery/.idea/CSV_Files/WGUPS Distance Table.csv')
+myMatrix = loadDistanceData('.idea/CSV_Files/WGUPS Distance Table.csv')
 
-# Load packages to Hash Table
-loadPackageData('C:/Users/Peter/IdeaProjects/C950_Package_Delivery/.idea/CSV_Files/WGUPS Package File.csv', myHash)
+# Print Matrix data
 printDistanceMatrix(myMatrix)
 
+# Load packages to Hash Table
+loadPackageData('.idea/CSV_Files/WGUPS Package File.csv', myHash)
 
+# Print header
 print("Packages from Hashtable:")
-# Fetch data from Hash Table
+
+# Print data from Hash Table
 for i in range (int((getNumberOfItemsFromMyHash(myHash.table))/2)):
-    print("Package: {}".format(myHash.search(i+1))) # 1 to 11 is sent to myHash.search()
+    print("Package: {}".format(myHash.search(i+1))) # 1 to 40 is sent to myHash.search()
+for i in range(1, 27):
+    first_package_data = myHash.search(i)
+    first_package_data.get_address()
